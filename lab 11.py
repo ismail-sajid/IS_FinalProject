214b71d8d64f7a735e99125024ffcb03f12726d9
ismail-sajid <117584253+ismail-sajid@users.noreply.github.com>
2023-01-17 22:39:19 +0500

    Add files via upload

--- lab 11.py
+++ lab 11.py
@@ -1,2 +1,14 @@
-ff = open("dataset_sunny.txt", "r")
+ff = open("dataset_sunny.csv", "r")
+data= ff.readlines()
+def classifier():
+    
+    for i in data:
+        dataset= i.split()
+        print(dataset)
+        
+        if dataset==['sunny,hot,high,FALSE,no']:
+            print("hello")
+            
+
+classifier()
 
